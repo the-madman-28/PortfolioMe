8afb9d47021f6f29090fae4bfa48abda5eea1269
Nitin Babu <68281837+the-madman-28@users.noreply.github.com>
2023-07-02 16:36:53 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
               specializing in Web Development, Software Development, and Database Management. Proficient in multiple programming languages 
               and frameworks, I aim to contribute to innovative projects by leveraging my strong foundation, curiosity for emerging 
               technologies, and dedication to delivering high-quality solutions."</p>
-              <a href='https://drive.google.com/file/d/1F1U9Ru9vyLmTPlVxA11qCXYJ8DiXfjoM/view?usp=sharing' className='btn'>Download Resume</a>
+              <a href='https://drive.google.com/file/d/1xX7kIbM9rF4g8RjArGipoOsFPVc7fkEY/view?usp=sharing' className='btn'>Download Resume</a>
             </div>
 
             <div className='about_skills grid'>
